e2006ef7197664ab1756e64b0f0512b9b5cde89d
WhimsicalDragon <40681291+WhimsicalDragon@users.noreply.github.com>
2022-06-13 15:48:28 -0500

    Example python file you can test the program on!

---
+++ example.py
@@ -0,0 +1,8 @@
+print("Hello World")
+if 1==1:
+    print("Example")
+    if 2 == 2:
+        pass #TODO
+else:
+    print("This message should not be displayed")
+pass #Something else should happen here later
